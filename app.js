3206c910f48b581597297233124ad6716a24e880
andrey.nikolayev <andrey.nikolayev@trinetix.com>
2019-05-23 17:49:07 +0300

    Added base request to telegram

--- app.js
+++ app.js
@@ -1,12 +1,27 @@
 var request = require('request');
 var cheerio = require('cheerio');
+var config = require('./config');
 
 var mangaUrl = 'https://mangalib.me/onepunchman';
 
 request(mangaUrl, function(err, resp, html) {
-        if (!err){
-          const $ = cheerio.load(html);
-          const latestChapter = $('.chapter-item a').first().text();
-          console.log(latestChapter); 
-      }
+  if (err){
+    console.log(err);
+  }
+  const $ = cheerio.load(html);
+  const latestChapter = $('.chapter-item a').first().text();
+  const telegramMessage = 'Последняя вышедшая глава Ванпанчмена: "'+latestChapter+'"';
+
+  request.post({
+    uri: 'https://api.telegram.org/bot'+ config.telegramBotToken +'/sendMessage',
+    form: {
+      chat_id: config.telegramId,
+      text: telegramMessage
+    }
+  }, function(err, resp, body) {
+    if(err) {
+      console.log(err);
+    }
+    console.log(resp);
+  });
 });
